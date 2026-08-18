b5c5b1ba252b15cee2f5dd2dda942299066fd57e
do7amo7amed <dohahabeb15@gmail.com>
2026-08-18 14:37:42 +0300

    Edit validator.js

---
+++ src/middlewares/validator.js
@@ -0,0 +1,17 @@
+const validate = (schema) => {
+    return (req, res, next) => {
+        const result = schema.safeParse(req.body);
+        if (!result.success){
+            return res.status(400).json({
+                success: false,
+                message: "validation failed",
+                errors: result.error.issues
+            });
+
+        }
+        req.body = result.data; // data after validaition and transform
+        next();
+    };
+};
+
+module.exports = validate;
