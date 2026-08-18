b619ae0b383510576a2b09b82a6b63c9c6fa9e28
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-18 23:42:37 +0300

    Implemented User service for db interactions

---
+++ src/services/userService.js
@@ -0,0 +1,19 @@
+const User = require('../models/userModel');
+
+const findUserByEmail = async (email) => {
+  return await User.findOne({ email });
+};
+
+const createUser = async (userData) => {
+  return await User.create(userData);
+};
+
+const findUserById = async (id) => {
+  return await User.findById(id).select('-password');
+};
+
+module.exports = {
+  findUserByEmail,
+  createUser,
+  findUserById,
+};
