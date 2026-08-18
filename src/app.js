e31386f5339d5120f63de6b40ec5559715584d2a
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-18 23:44:38 +0300

    Added health check endpoint & auth routes

--- src/app.js
+++ src/app.js
@@ -8,10 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
 app.use('/api/resources', resourcesRoutes);
+
+app.use('/api/auth', authRoutes);
 
 app.use(notFoundMiddleware)
 
 app.use(errorHandler);
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', message: 'Server is healthy' });
+});
 
 module.exports = app;
