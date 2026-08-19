461db6f4e6cf84f6078c10a7dbb31466003fb74b
Salma-Islam1 <iholo6529@gmail.com>
2026-08-19 13:29:02 +0300

    the code for the routing in courses

---
+++ src/routes/courseRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const { authenticate, authorize } = require("../middlewares");
+const { validate } = require("../middlewares/validator");
+const courseSchema = require("../validators/courseSchema");
+
+const {
+  getAllCourses,
+  getCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} = require("../controllers/courseController");
+
+router.get("/", getAllCourses);
+
+router.get("/:courseCode", getCourse);
+
+router.post("/", authenticate, authorize(['admin']), validate(courseSchema), createCourse);
+
+router.put("/:courseCode", authenticate, authorize(['admin']), validate(courseSchema), updateCourse);
+
+router.delete("/:courseCode", authenticate, authorize(['admin']), deleteCourse);
+
+module.exports = router;
