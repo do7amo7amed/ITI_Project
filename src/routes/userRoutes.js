210cf518f38aa7117bcb5cfa149666e939a68937
Mohamed Ehab Mohamed <123424326+mido-ehab11@users.noreply.github.com>
2026-08-19 17:04:26 +0300

    Create userRoutes.js

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const authenticate = require('../middlewares/auth');
+const authorize = require('../middlewares/authorize');
+
+router.use(authenticate);
+
+
+router.route('/profile')
+  .get(userController.getProfile)
+  .put(
+   
+    userController.updateProfile
+  );
+
+
+router.route('/:id')
+  .delete(
+    authorize('admin'), 
+    userController.deleteUser
+  );
+
+module.exports = router;
