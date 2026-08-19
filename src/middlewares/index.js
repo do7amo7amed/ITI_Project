30b3d357a495a815fd4afdcdc05f82d30522ee24
Salma-Islam1 <iholo6529@gmail.com>
2026-08-19 13:28:18 +0300

    making validate being read by index

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,14 +1,14 @@
 const auth= require('./auth');
 const logger = require('./logger');
 const errorHandler = require('./errorHandler');
-const validator = require('./validator');
+const validate = require('./validator');
 const notFoundMiddleware = require('./notFoundMiddleware');
 //const authorize = require('./authorize');
 module.exports = {
     auth,
     logger,
     errorHandler,
-    validator,
+    validate,
     notFoundMiddleware ,
 //    authorize
 }
