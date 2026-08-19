908185bf5ba271a0500835871262ea86e5ea9256
Salma-Islam1 <iholo6529@gmail.com>
2026-08-19 13:34:00 +0300

    added authorize in index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,12 +3,12 @@ const logger = require('./logger');
 const errorHandler = require('./errorHandler');
 const validate = require('./validator');
 const notFoundMiddleware = require('./notFoundMiddleware');
-//const authorize = require('./authorize');
+const authorize = require('./authorize');
 module.exports = {
     auth,
     logger,
     errorHandler,
     validate,
     notFoundMiddleware ,
-//    authorize
+    authorize
 }
