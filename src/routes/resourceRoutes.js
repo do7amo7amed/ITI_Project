16c8255a880bfcf13728b243609ac131aec5eab5
do7amo7amed <dohahabeb15@gmail.com>
2026-08-20 01:25:14 +0300

    adding auth for resources

--- src/routes/resourceRoutes.js
+++ src/routes/resourceRoutes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const resourceController = require("../controllers/resourceController");
+const upload = require("../middlewares/uploads");
 
 const {createResourceSchema,updateResourceSchema} = require("../validators/resourcesSchema");
 const {validate} = require("../middlewares/validator");
+const  {auth,authorize}  = require("../middlewares");
+
 
 router.route("/")
 .get(resourceController.getAllResources)
-.post(validate(createResourceSchema), resourceController.createResource);
+.post(auth,authorize('admin'),upload.single("file"),validate(createResourceSchema), resourceController.createResource);
 router.route("/:id")
 .get(resourceController.getSingleResource)
-.put( validate(updateResourceSchema),resourceController.updateResource)
-.delete(resourceController.deleteResource);
+.put( auth,authorize('admin'),upload.single("file"),validate(updateResourceSchema),resourceController.updateResource)
+.delete(auth,resourceController.deleteResource);
 
 module.exports = router;
