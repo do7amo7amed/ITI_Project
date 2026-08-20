915c20b6d319c65d555c1ba54e8bb3cd6033f70f
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-20 19:44:15 +0300

    Add multer and cloudinary storage to uploads middleware

--- src/middlewares/uploads.js
+++ src/middlewares/uploads.js
@@ -1,3 +1,4 @@
+//src/middlewares/uploads.js
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../services/cloudinary");
