27300b7f209cda21a603b3c0b76d152eedb729f8
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-20 19:43:25 +0300

    Fix formatting and add newline at end of index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,14 +1,18 @@
-const auth= require('./auth');
+//src/middlewares/index.js
+//makes every middleware available in one place to import anywhere
+
+const auth = require('./auth');
 const logger = require('./logger');
 const errorHandler = require('./errorHandler');
 const validate = require('./validator');
 const notFoundMiddleware = require('./notFoundMiddleware');
 const authorize = require('./authorize');
+
 module.exports = {
     auth,
     logger,
     errorHandler,
     validate,
-    notFoundMiddleware ,
+    notFoundMiddleware,
     authorize
-}
+};
