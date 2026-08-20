6b276a10d6983bfd49031a29152fb74b16e32630
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-20 19:36:47 +0300

    Change academicLevel to be an integer type

--- src/validators/courseValidator.js
+++ src/validators/courseValidator.js
@@ -1,3 +1,4 @@
+//src/validators/courseValidator.js
 const z= require('zod');
 
 const courseSchema = z.object({
@@ -7,8 +8,11 @@ const courseSchema = z.object({
     courseCode : z.string({ required_error: 'Course Code is required' })
     .min(5, "Course code must be at least 5 characters long").max(10, "Course code can't be more than 10 characters "),
     
-    academicLevel: z.string({ required_error: 'Academic Level is required' })
-    .min(6, 'Academic level must be at least 6 characters long').max(10, 'Academic level cant be more than 10 characters'),
+    academicLevel: z
+    .number({ required_error: 'Academic Level is required' })
+    .int('Academic level must be an integer')
+    .min(1, 'Academic level must be between 1 and 4')
+    .max(4, 'Academic level must be between 1 and 4'),
     
     semester: z.string({ required_error: 'Semester is required' })
     .min(4, 'Semester must be at least 4 characters long')
