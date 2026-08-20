90b9d02279bbde17d3cac8ca9f1d14bffa82c8a9
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-20 19:37:52 +0300

    Refactor responseHandler by removing unused functions
    Removed sendSuccess and sendError functions from responseHandler.js.

--- src/utils/responseHandler.js
+++ src/utils/responseHandler.js
@@ -1,5 +1,6 @@
+//src/utils/responseHandler.js
 const responseHandler = (res, statusCode, message, data = null) => {
-  const isSuccess = statusCode >= 200 && statusCode < 300;
+const isSuccess = statusCode >= 200 && statusCode < 300;
 
   res.status(statusCode).json({
     success: isSuccess,
@@ -10,23 +11,6 @@ const responseHandler = (res, statusCode, message, data = null) => {
   });
 };
 
-const sendSuccess = (res, data, message, statusCode) => {
-   return res.status(statusCode).json({
-       success: true,
-       message,
-       data
-   });
-};
-
-const sendError = (res, message, statusCode) => {
-   return res.status(statusCode).json({
-       success: false,
-       message
-   });
-}
-
 module.exports = {
     responseHandler,
-   sendSuccess,
-   sendError
 }
