8439b4da4f15b3d074ba273727ed9c422c2919a4
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-20 19:34:57 +0300

    Reformat server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+//server.js
 //starts the app, connects db
 
 //import third-party library to read .env 
-require('dotenv').config();
+require('dotenv').config(); 
 
 const app = require('./src/app');
 const connectDB = require('./src/services/dbConfig');
 
 //start mongoDB connection
-connectDB();
+connectDB(); 
 
 //start http server listening for requests on this port
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 3000, () => { 
+  console.log(`Server is running on port ${process.env.PORT || 3000}`); 
 });
