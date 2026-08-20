8b37edcc42a4855e4edc4246a3ef94833a243646
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-20 19:35:59 +0300

    Change academicLevel max value from 5 to 4

--- src/validators/userProfileSchema.js
+++ src/validators/userProfileSchema.js
@@ -1,14 +1,15 @@
+//src/validators/userProfileSchema.js
+
 const { z } = require("zod");
 
 const updateProfileSchema = z.object({
   name: z.string().min(2).max(50).trim().optional(),
   university: z.string().trim().optional(),
   department: z.string().trim().optional(),
-  academicLevel: z.number().int().min(1).max(5).optional(),
+  academicLevel: z.number().int().min(1).max(4).optional(),
   profileInformation: z
     .object({
       bio: z.string().optional(),
-      avatar: z.string().optional(),
     })
     .optional(),
 });
