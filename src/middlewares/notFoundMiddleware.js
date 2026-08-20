1039f881274c2fa3288f90446b3eab103b31a935
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-08-20 19:43:49 +0300

    Refactor notFoundMiddleware for error handling
    Refactor notFoundMiddleware to create an error object and pass it to next()

--- src/middlewares/notFoundMiddleware.js
+++ src/middlewares/notFoundMiddleware.js
@@ -1,7 +1,12 @@
+//src/middlewares/notFoundMiddleware.js
+// handles unhandled errors
+
 const notFoundMiddleware = (req, res, next) => {
-    res.status(404).json({
-        success: false,
-        message: '404 Not Found'
-    });
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  error.status = 404;
+  error.name = 'NotFoundError';
+  res.status(404);
+  next(error);
 };
+
 module.exports = notFoundMiddleware;
